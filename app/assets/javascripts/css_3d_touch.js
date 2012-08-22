5dfe4d242c504c02351a36f421f3027ec391a512
Tatsuya Aizawa <Tatsuya.Aizawa@jrits.ricoh.co.jp>
2012-08-22 11:38:25 +0900

    デバイス回転時の画像サイズ調整

--- app/assets/javascripts/css_3d_touch.js
+++ app/assets/javascripts/css_3d_touch.js
@@ -27,4 +27,35 @@ function init()
   } else {
     $("#loading").text("CSS/3D is not supported.");
   }
+
+  set_layout();
+  $(window).bind("resize", set_layout);
+}
+
+function set_layout()
+{
+  var menu_space;
+  var image_height;
+  var image_width;
+
+  var $html = $('html');
+  var $auto_rotate_button = $('.css3_3d_transform.touch .menu');
+  var $social_button = $('.menu.css_touch');
+
+  if($html.innerHeight() < $html.innerWidth()){
+    $html.addClass('landscape');
+    menu_space = $auto_rotate_button.outerWidth(true);
+    image_height = $html.innerHeight();
+    image_width = $html.innerWidth() - menu_space;
+  }else{
+    $html.removeClass('landscape');
+    menu_space = $auto_rotate_button.outerHeight(true) + $social_button.outerHeight(true);
+    image_height = $html.innerHeight() - menu_space;
+    image_width = $html.innerWidth();
+  }
+  var image_properties = {'height': image_height+'px', 'width': image_width+'px'};
+  $(".body, #container, #cube, #controller").css(image_properties);
+
+  setTimeout(scrollTo, 100, 0, 1); //hide url bar
+  build_texture();
 }
