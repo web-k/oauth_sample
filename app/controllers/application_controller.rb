f6b4297230068ef3354e3a5d627e63bd6f51b941
Toshiharu Takematsu <info@newfish.jp>
2012-10-11 23:37:55 +0900

    User-Agentのログ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_user_agent
   
   include ApplicationHelper
   
   def check_user_agent
     if @user_agent.blank?
-      @user_agent = MobileESP::UserAgentInfo.new user_agent(request), accept(request)
+      user_agent = user_agent(request)
+      logger.info 'UA: ' + user_agent.inspect
+      @user_agent = MobileESP::UserAgentInfo.new user_agent, accept(request)
       @is_ios = @user_agent.is_iphone || @user_agent.detect_ipad
     end
   end
