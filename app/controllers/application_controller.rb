8a383c3128fb7120104f7c61502b0d65f2677feb
Toshiharu Takematsu <info@newfish.jp>
2013-04-30 12:38:14 +0900

    リクエストヘッダ情報をログに表示(Accept-Language等)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :check_user_agent
+  before_filter :check_request_headers
   
   include ApplicationHelper
   
   def check_user_agent
     if @user_agent.blank?
       user_agent = user_agent(request)
-      logger.info 'UA: ' + user_agent.inspect
       @user_agent = MobileESP::UserAgentInfo.new user_agent, accept(request)
       @is_ios = @user_agent.is_iphone || @user_agent.detect_ipad
     end
   end
   
+  def check_request_headers
+    logger.info 'Headers:'
+    user_agent = user_agent(request)
+    logger.info '  User-Agent: ' + user_agent.inspect
+    logger.info '  Accept: ' + request.headers['Accept'].inspect
+    logger.info '  Accept-Language: ' + request.headers['Accept-Language'].inspect
+    logger.info '  Accept-Encoding: ' + request.headers['Accept-Encoding'].inspect
+    logger.info '  Cache-Control: ' + request.headers['Cache-Control'].inspect
+  end
+
   def ios?
     check_user_agent
     @is_ios
